e9aec32f83005441f32ba767ea6177bd84a7cfec
Egor Ermakov <ermakov.em@phystech.edu>
2024-11-12 20:31:58 +0000

    total rewrite as first attempt did not sort

--- hw_1.cpp
+++ hw_1.cpp
@@ -7,80 +7,45 @@
 #include <parlay/parallel.h>
 #include <parlay/sequence.h>
 
+
 #define SEQ_SIZE 100'000'000
-#define processors 4
+#define CHUNK_SIZE 1000
 
+int split(parlay::sequence<int>& s, int begin, int end) {
+    int pivot = s[end];
+    int index = begin - 1;
 
-void seq_sort(parlay::sequence<int>& input, int left, int right) {
-    // if (left < right) {
-    //     std::sort(input.begin() + left, input.begin() + right + 1);
-    // }
-    int i = left;
-    int j = right;
-    int pivot = input[(left + right) / 2];
-
-    // Разделение на подмассивы
-    while (i <= j) {
-        while (input[i] < pivot)
-            i++;
-        while (input[j] > pivot)
-            j--;
-        if (i <= j) {
-            std::swap(input[i], input[j]);
-            i++;
-            j--;
+    for (int current = begin; current < end; current++) {
+        if (s[current] <= pivot) {
+            index++;
+            std::swap(s[index], s[current]);
         }
     }
 
-    if (left < j)
-        seq_sort(input, left, j);
-    if (i < right)
-        seq_sort(input, i, right);
+    std::swap(s[index + 1], s[end]);
+    return index + 1;
 }
 
-parlay::sequence<int> merge_two(const parlay::sequence<int>& a, const parlay::sequence<int>& b) {
-    parlay::sequence<int> merged(a.size() + b.size());
-    size_t idx = 0, jdx = 0, kdx = 0;
-
-    while(idx < a.size() && jdx < b.size()){
-        if(a[idx] < b[jdx]){
-            merged[kdx++] = a[idx++];
-        }
-        else{
-            merged[kdx++] = b[jdx++];
-        }
+void seq_sort(parlay::sequence<int>& s, int begin, int end) {
+    if (begin < end) {
+        int idx = split(s, begin, end);
+        seq_sort(s, begin, idx - 1);
+        seq_sort(s, idx + 1, end);
     }
-
-    while(idx < a.size()){
-        merged[kdx++] = a[idx++];
-    }
-    while(jdx < b.size()){
-        merged[kdx++] = b[jdx++];
-    }
-
-    return merged;
 }
 
-parlay::sequence<int> parallel_merge(parlay::sequence<parlay::sequence<int>>& chunks) {
-    if(chunks.empty()) return parlay::sequence<int>();
-    while(chunks.size() > 1){
-        size_t new_size = (chunks.size() + 1) / 2;
-        parlay::sequence<parlay::sequence<int>> new_chunks(new_size);
-
-        parlay::parallel_for(0, new_size, [&](size_t i){
-            size_t first = 2 * i;
-            size_t second = 2 * i + 1;
-            if(second < chunks.size()){
-                new_chunks[i] = merge_two(chunks[first], chunks[second]);
-            }
-            else{
-                new_chunks[i] = chunks[first];
-            }
-        });
-
-        chunks = std::move(new_chunks);
+void par_sort(parlay::sequence<int>& s, int begin, int end) {
+    if (end - begin < CHUNK_SIZE) {
+        seq_sort(s, begin, end);
+    } else {
+        int idx = split(s, begin, end);
+        //par_sort(v, begin, idx - 1);
+        //par_sort(v, idx + 1, end);
+        parlay::par_do(
+            [&]() { par_sort(s, begin, idx - 1); },
+            [&]() { par_sort(s, idx + 1, end); }
+        );
     }
-    return chunks[0];
 }
 
 parlay::sequence<int> sequence_generator(int n) {
@@ -100,40 +65,11 @@ parlay::sequence<int> sequence_generator(int n) {
     });
 }
 
-parlay::sequence<parlay::sequence<int>> split_sequence(const parlay::sequence<int>& input, size_t max_chunk_size = 1000) {
-    size_t n = input.size();
-    if (n == 0) return parlay::sequence<parlay::sequence<int>>();
-
-    size_t num_chunks = (n + max_chunk_size - 1) / max_chunk_size;
-
-    parlay::sequence<parlay::sequence<int>> output(num_chunks);
-
-    parlay::parallel_for(0, num_chunks, [&](size_t i) {
-        size_t start = i * max_chunk_size;
-        size_t end = std::min(start + max_chunk_size, n);
-        output[i] = parlay::sequence<int>(&input[start], &input[end]);
-    });
-
-    return output;
-}
-
-parlay::sequence<int> par_sort(const parlay::sequence<int>& input) {
-    parlay::sequence<parlay::sequence<int>> chunks = split_sequence(input, 1000);
-
-    parlay::parallel_for(0, chunks.size(), [&](size_t i) {
-        seq_sort(chunks[i], 0, chunks[i].size() - 1);
-    });
-
-    parlay::sequence<int> sorted = parallel_merge(chunks);
-
-    return sorted;
-}
-
 
 int main() {
 
     //warm up, without it first par_sort result will not be representative
-    par_sort(sequence_generator(SEQ_SIZE));
+    //par_sort(sequence_generator(SEQ_SIZE));
 
     std::vector<parlay::sequence<int>> pregen_seqs;
 
@@ -148,9 +84,13 @@ int main() {
         auto seq = pregen_seqs[i];
         auto start = std::chrono::high_resolution_clock::now();
 
-        par_sort(seq);
+        par_sort(seq, 0, seq.size() - 1);
         
         auto end = std::chrono::high_resolution_clock::now();
+        if (!std::is_sorted(seq.begin(), seq.end())) {
+            std::cout << "NOT SORTED!!!" << std::endl;
+            return 0;
+        }
         std::chrono::duration<double> elapsed = end - start;
         par_avg_counter += elapsed.count();
         std::cout << '\t' << i + 1 << ". Время выполнения: " << elapsed.count() << " секунд" << std::endl;
@@ -166,6 +106,10 @@ int main() {
         seq_sort(seq, 0, seq.size() - 1);
 
         auto end = std::chrono::high_resolution_clock::now();
+        if (!std::is_sorted(seq.begin(), seq.end())) {
+            std::cout << "NOT SORTED!!!" << std::endl;
+            return 0;
+        }
         std::chrono::duration<double> elapsed = end - start;
         seq_avg_counter += elapsed.count();
         std::cout << '\t' << i + 1 << ". Время выполнения: " << elapsed.count() << " секунд" << std::endl;
